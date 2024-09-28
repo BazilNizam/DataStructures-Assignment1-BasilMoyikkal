f712a2def6b2420a2f1583c617e3016f2698a198
Bazil <bazil4u@gmail.com>
2024-09-28 19:50:50 -0400

    added comments :pencil2:

--- src/main/java/scrollstack/Main.java
+++ src/main/java/scrollstack/Main.java
@@ -1,31 +1,44 @@
+// Author: Basil Moyikkal
+// ID: C0908488
+
 package scrollstack;
 
 public class Main {
     public static void main(String[] args) {
-        // Create an instance of ScrollStack
+        // create an instance of ScrollStack
+        // this stack will hold our scrolls
         ScrollStack stack = new ScrollStack();
 
-        // Push some scrolls onto the stack
+        // push some scrolls onto the stack
+        // adding first scroll to the stack
         stack.pushScroll("Ancient Scroll of Wisdom");
+        // adding second scroll to the stack
         stack.pushScroll("Scroll of the Forbidden Temple");
+        // adding third scroll to the stack
         stack.pushScroll("Scroll of the Lost City");
 
-        // Peek at the top scroll
+        // peek at the top scroll
+        // this will show the scroll that is currently at the top of the stack
         stack.peekScroll();
 
-        // Check if a specific scroll is in the stack
+        // check if a specific scroll is in the stack
+        // looking for the 'Scroll of the Forbidden Temple'
         stack.containsScroll("Scroll of the Forbidden Temple");
 
-        // Pop the top scroll off the stack
+        // pop the top scroll off the stack
+        // this will remove the scroll currently at the top
         stack.popScroll();
 
-        // Peek again at the top scroll
+        // peek again at the top scroll
+        // check which scroll is now at the top after popping one off
         stack.peekScroll();
 
-        // Print all remaining scrolls
+        // print all remaining scrolls
+        // this will display all scrolls that are still in the stack
         stack.printAllScrolls();
 
-        // Check for a scroll that was removed
+        // check for a scroll that was removed
+        // looking for 'Scroll of the Lost City', which should still be in the stack
         stack.containsScroll("Scroll of the Lost City");
     }
 }
--- src/main/java/scrollstack/ScrollStack.java
+++ src/main/java/scrollstack/ScrollStack.java
@@ -1,58 +1,75 @@
+// Author: Basil Moyikkal
+// ID: C0908488
+
 package scrollstack;
 
 import java.util.Stack;
 
+// Class representing a stack of scrolls
 public class ScrollStack {
-    // We will use the built-in Java Stack class to manage the scrolls
+    // we will use the built-in Java Stack class to manage the scrolls
     private Stack<String> stack;
 
     // Constructor to initialize the scroll stack
     public ScrollStack() {
+        // initializing the stack for storing scroll titles
         stack = new Stack<>();
     }
 
     // Push a new scroll onto the stack
     public void pushScroll(String scrollTitle) {
+        // adding the scroll title to the top of the stack
         stack.push(scrollTitle);
+        // informing that the scroll has been added
         System.out.println("Added scroll: " + scrollTitle);
     }
 
     // Pop the top scroll off the stack
     public void popScroll() {
+        // checking if the stack is empty before attempting to pop
         if (stack.isEmpty()) {
+            // no scrolls to remove, so print a message
             System.out.println("No scrolls to remove.");
         } else {
+            // removing the scroll at the top of the stack
             String removedScroll = stack.pop();
+            // informing which scroll has been removed
             System.out.println("Removed scroll: " + removedScroll);
         }
     }
 
     // Peek at the top scroll without removing it
     public void peekScroll() {
-        // Check if the stack is empty
+        // check if the stack is empty
         if (stack.isEmpty()) {
+            // print a message if there are no scrolls
             System.out.println("No scrolls in the stack.");
         } else {
+            // displaying the scroll currently at the top of the stack
             System.out.println("Top scroll: " + stack.peek());
         }
     }
 
     // Check if a specific scroll title exists in the stack
     public void containsScroll(String scrollTitle) {
-        // Use the contains method of the Stack class
+        // use the contains method of the Stack class to check for the scroll
         if (stack.contains(scrollTitle)) {
+            // informing that the scroll exists in the stack
             System.out.println("Scroll \"" + scrollTitle + "\" exists in the stack.");
         } else {
+            // informing that the scroll does not exist in the stack
             System.out.println("Scroll \"" + scrollTitle + "\" is not in the stack.");
         }
     }
 
     // Print all scrolls in the stack
     public void printAllScrolls() {
-        // Check if the stack is empty
+        // check if the stack is empty
         if (stack.isEmpty()) {
+            // print a message if the stack is empty
             System.out.println("The scroll stack is empty.");
         } else {
+            // displaying all scrolls currently in the stack
             System.out.println("Scrolls in the stack: " + stack);
         }
     }
